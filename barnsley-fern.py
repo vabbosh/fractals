372acc818daae4b4dc01e2687a2175d4a7818e61
Vincent Abbosh <vabbosh@gmail.com>
2019-08-12 18:27:16 +1000

    The fern using Numpy and Transform matrices

--- barnsley-fern.py
+++ barnsley-fern.py
@@ -1,8 +1,26 @@
 # importing necessary modules
 import matplotlib.pyplot as plt
-from random import randint
+import numpy as np
 
-# initializing the list
+# initializing the affine transform
+stem = np.array([[0, 0, 0], [0, 0.16, 0], [0, 0, 1]])
+small_leaflet = np.array([[0.85, 0.04, 0], [-0.04, 0.85, 1.6], [0, 0, 1]])
+large_L_leaflet = np.array([[0.2, -0.26, 0], [0.23, 0.22, 1.6], [0, 0, 1]])
+large_R_leaflet = np.array([[-0.15, 0.28, 0], [0.26, 0.24, 0.44], [0, 0, 1]])
+
+delta = 0.01
+P = np.array([max(delta, np.abs(np.linalg.det(stem))),
+              max(delta, np.abs(np.linalg.det(small_leaflet))),
+              max(delta, np.abs(np.linalg.det(large_L_leaflet))),
+              max(delta, np.abs(np.linalg.det(large_R_leaflet)))])
+
+P = P / P.sum()
+
+frac_ops = [lambda p: np.dot(stem, p),
+            lambda p: np.dot(small_leaflet, p),
+            lambda p: np.dot(large_L_leaflet, p),
+            lambda p: np.dot(large_R_leaflet, p)]
+
 x = []
 y = []
 
@@ -15,34 +33,13 @@ current = 0
 for i in range(1, 500000):
 
     # generates a random integer between 1 and 100
-    z = randint(1, 100)
-
-    # the x and y coordinates of the equations
-    # are appended in the lists respectively.
-
-    # for the probability 0.01
-    if z == 1:
-        x.append(0)
-        y.append(0.16 * (y[current]))
-
-    # for the probability 0.85
-    if z >= 2 and z <= 86:
-        x.append(0.85 * (x[current]) + 0.04 * (y[current]))
-        y.append(-0.04 * (x[current]) + 0.85 * (y[current]) + 1.6)
-
-    # for the probability 0.07
-    if z >= 87 and z <= 93:
-        x.append(0.2 * (x[current]) - 0.26 * (y[current]))
-        y.append(0.23 * (x[current]) + 0.22 * (y[current]) + 1.6)
-
-    # for the probability 0.07
-    if z >= 94 and z <= 100:
-        x.append(-0.15 * (x[current]) + 0.28 * (y[current]))
-        y.append(0.26 * (x[current]) + 0.24 * (y[current]) + 0.44)
+    newPoint = np.random.choice(frac_ops, p=P)([x[current], y[current], 1])
+    x.append(newPoint[0])
+    y.append(newPoint[1])
 
     current = current + 1
 
-plt.figure(figsize=(10,20))
-plt.scatter(x, y, s=0.2, edgecolor='green')
+plt.figure(figsize=(10, 20))
+plt.scatter(x[100:], y[100:], s=0.2, edgecolor='green')
 
 plt.show()
